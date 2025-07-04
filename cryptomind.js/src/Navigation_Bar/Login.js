ca1ab41e0fdbbc62d66000f8cdc2765ae0bcfce9
AdelinChaushev <106106846+AdelinChaushev@users.noreply.github.com>
2025-07-04 18:55:37 +0300

    Added Login and Register

--- cryptomind.js/src/Navigation_Bar/Login.js
+++ cryptomind.js/src/Navigation_Bar/Login.js
@@ -1,10 +1,32 @@
-import axios from "axios"
-export default function Login(){
-    
-    
-    return(
-        <button>
-            Login
-        </button>
-    )
-}
+import React, { useState } from 'react';
+import axios from 'axios';
+import './styles/Login.css';
+
+export default function Login () {
+
+    const [state,setState] = useState({email: '', password:''})
+   const  onChangeState = (e) =>  {
+       setState({...state,[e.target.name]:e.target.value})
+   }
+   const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("IN submit")
+    axios.post('http://localhost:5115/api/User/login',{
+        email: state.email,
+        password: state.password,
+
+    }).then(res => console.log(res.data)).catch(e => console.log(e));   
+   }
+
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Log In</h2>
+        <input type="email" name="email" placeholder="Email" value={state.email} onChange={onChangeState} required />
+        <input type="password" name="password" placeholder="Password" value={state.password} onChange={onChangeState} required />
+        <button type="submit"   >Login</button>
+      </form>
+    </div>
+  );
+};
+
