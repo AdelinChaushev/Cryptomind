735b0b1432787e75f34c424abfed17d8fc3717e3
Samuil Gigov <samuilgigov@gmail.com>
2025-07-05 14:58:46 +0300

    Added 2 more CRUD operations for admins, updating and deleting already approved ciphers. Have to prevent to submit, approve, update ciphers that will have the same title as other cipher.

--- Cryptomind.Common/CipherAdminViewModels/ApproveUpdateCipherViewModel.cs
+++ Cryptomind.Common/CipherAdminViewModels/ApproveUpdateCipherViewModel.cs
@@ -8,10 +8,9 @@ using System.Threading.Tasks;
 
 namespace Cryptomind.Common.CipherAdminViewModels
 {
-	public class ApproveCipherViewModel
+	public class ApproveUpdateCipherViewModel
 	{
 		public string Title { get; set; }
-		public string DecryptedText { get; set; }
 		//public CipherType TypeOfCipher { get; set; }
 		public bool AllowHint { get; set; }
 		public bool AllowSolution { get; set; }
