e94c79789e8e6c0ef07ef0155c031b685a887f8c
Samuil Gigov <samuilgigov@gmail.com>
2025-07-05 14:58:46 +0300

    Added 2 more CRUD operations for admins, updating and deleting already approved ciphers. Have to prevent to submit, approve, update ciphers that will have the same title as other cipher.

--- Crytomind.Core/Services/AdminService.cs
+++ Crytomind.Core/Services/AdminService.cs
@@ -13,28 +13,83 @@ namespace Crytomind.Core.Services
 	public class AdminService (
 		IRepository<Cipher, int> cipherRepo) : IAdminService
 	{
-
-		public async Task<List<Cipher>> AllSubmittedCyphers()
+		public async Task<List<Cipher>> AllSubmittedCiphers()
 		{
 			var result = (await cipherRepo.GetAllAsync()).Where(c => c.IsApproved == false).ToList();
 			if (result == null) throw new InvalidOperationException("Wasn't able to retrieve submitted ciphers");
 			return result;
 		}
-		public async Task<Cipher> GetCipherById(int id) //Should I return cipher the current logic is to return it so it can be shown to the next view
+		public async Task<List<Cipher>> AllApprovedCiphers()
+		{
+			var result = (await cipherRepo.GetAllAsync()).Where(c => c.IsApproved == true).ToList();
+			if (result == null) throw new InvalidOperationException("Wasn't able to retrieve approved ciphers");
+			return result;
+		}
+		public async Task<Cipher> GetCipherById(int id) 
 		{
 			Cipher? cipher = await cipherRepo.GetByIdAsync(id);
 
 			if (cipher == null) throw new InvalidOperationException("There is no cipher with the given Id");
 			return cipher; //Go to next
 		}
-
-		public async Task ApproveCipherAsync(int id, ApproveCipherViewModel model)
+		public async Task ApproveCipherAsync(int id, ApproveUpdateCipherViewModel model) 
 		{
-			Cipher? cipher = await cipherRepo.GetByIdAsync(id);
+			Cipher? cipher = await GetCipherById(id);
 			if (cipher == null) throw new InvalidOperationException("There is no cipher with the given Id");
+			else if (cipher.IsApproved) throw new InvalidOperationException("The cipher with the given Id is already approved");
 
+			DefineTags(cipher, model.Tags.ToList());
+
+			cipher.Title = model.Title;
+			//result.TypeOfCipher = model.TypeOfCipher;
+			cipher.AllowHint = model.AllowHint;
+			cipher.AllowSolution = model.AllowSolution;
+			cipher.IsApproved = true;
+
+			await cipherRepo.UpdateAsync(cipher);
+			//await Console.Out.WriteLineAsync(await cipherRepo.UpdateAsync(cipher);.ToString());
+			
+			//Here there might be name collision with other cipher
+		}
+		public async Task RejectCipherAsync(int id)
+		{
+			Cipher? cipher = await GetCipherById(id);
+			if(cipher == null) throw new InvalidOperationException("There is no cipher with the given Id");
+			else if (cipher.IsApproved) throw new InvalidOperationException("The cipher with the given Id is already approved");
+
+			bool isDeleted = await cipherRepo.DeleteAsync(cipher);
+
+			if (!isDeleted) throw new InvalidOperationException("Wasn't able to delete the cipher");
+		}
+		public async Task DeleteApprovedCipher(int id)
+		{
+			Cipher? cipher = await GetCipherById(id);
+			if(cipher == null) throw new InvalidOperationException("There is no cipher with the given Id");
+			else if (!cipher.IsApproved) throw new InvalidOperationException("The cipher with the given Id is not approved");
+
+			bool result = await cipherRepo.DeleteAsync(cipher);
+			if (!result) throw new InvalidOperationException("Wasn't able to delete the cipher");
+		}
+		public async Task UpdateApprovedCipher(int id, ApproveUpdateCipherViewModel model)
+		{
+			Cipher? cipher = await GetCipherById(id);
+
+			if (cipher == null) throw new InvalidOperationException("There is no cipher with the given Id");
+			else if (!cipher.IsApproved) throw new InvalidOperationException("The cipher with the given Id is not approved");
+
+			cipher.Title = model.Title;
+			cipher.AllowHint = model.AllowHint;
+			cipher.AllowSolution = model.AllowSolution;
+			DefineTags(cipher, model.Tags.ToList());
+
+			await cipherRepo.UpdateAsync(cipher);
+		}
+
+		//Common methods
+		private void DefineTags (Cipher? cipher, List<Tag> tags)
+		{
 			List<CipherTag> cipherTags = new List<CipherTag>();
-			foreach (var tag in model.Tags)
+			foreach (var tag in tags)
 			{
 				var cipherTag = new CipherTag()
 				{
@@ -44,29 +99,7 @@ namespace Crytomind.Core.Services
 
 				cipherTags.Add(cipherTag);
 			}
-
 			cipher.CipherTags = cipherTags;
-
-			cipher.Title = model.Title;
-			cipher.DecryptedText = model.DecryptedText;
-			//result.TypeOfCipher = model.TypeOfCipher;
-			cipher.AllowHint = model.AllowHint;
-			cipher.AllowSolution = model.AllowSolution;
-			cipher.IsApproved = true;
-
-			bool isSuccessfull = await cipherRepo.UpdateAsync(cipher);
-            await Console.Out.WriteLineAsync(isSuccessfull.ToString());
-            //Here there might be name collision with other cipher
-        }
-
-		public async Task RejectCipherAsync(int id)
-		{
-			Cipher? cipher = await cipherRepo.GetByIdAsync(id);
-			if (cipher == null) throw new InvalidOperationException("There is no cipher with the given Id");
-
-			bool isDeleted = await cipherRepo.DeleteAsync(cipher);
-
-			if (!isDeleted) throw new InvalidOperationException("Wasn't able to delete the cipher");
 		}
 	}
 }
