21a9607f6f7176f87ef9a2569087812dd1fe873f
Samuil Gigov <samuilgigov@gmail.com>
2025-07-05 15:32:48 +0300

    Created a mapping table for ApplicationUser and Cipher entities

--- Cryptomind.Data/Enums/CipherType.cs
+++ Cryptomind.Data/Enums/CipherType.cs
@@ -10,6 +10,23 @@ namespace Cryptomind.Data.Enums
     {
         Caesar = 0,
         Substitution = 1,
-
-    }
+		Atbash = 2,
+		Affine = 3,
+		Vigenere = 4,
+		Playfair = 5,
+		RailFence = 6,
+		ColumnarTransposition = 7,
+		ROT13 = 8,
+		Baconian = 9,
+		PolybiusSquare = 10,
+		MorseCode = 11,
+		ADFGVX = 12,
+		Hill = 13,
+		Beaufort = 14,
+		Keyword = 15,
+		Nihilist = 16,
+		OneTimePad = 17,
+		Autokey = 18,
+		XOR = 19,
+	}
 }
