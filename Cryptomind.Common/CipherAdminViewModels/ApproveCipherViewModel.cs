cf9b3db6fa0c47a2daf07661afe0e2610ff162d2
Samuil Gigov <samuilgigov@gmail.com>
2025-07-05 13:00:42 +0300

    Implemented Admin Cipher operations, not fully there are still things that need to be done for the future, like updating a user's score after approving a cipher submitted by him. There are some key things like that we pass the db entity for approving because there the admin can change the title which was submitted by the user earlier etc. So we pass everything because it is an admin page and we think that our admin's won't troll, another reason for that is that we need all that for the user to see it but also not everything that will be passed will be changed. We can implement a DTO for that reason because it's really bad practice to reveal the whole entity to the front-end. When testing the this repo comment the attributes for authorization because there in production side we will need the user to be admin but right now we don't have admin role so when testing we have to disable the authorization.

---
+++ Cryptomind.Common/CipherAdminViewModels/ApproveCipherViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Cryptomind.Data.Entities;
+using Cryptomind.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptomind.Common.CipherAdminViewModels
+{
+	public class ApproveCipherViewModel
+	{
+		public string Title { get; set; }
+		public string DecryptedText { get; set; }
+		//public CipherType TypeOfCipher { get; set; }
+		public bool AllowHint { get; set; }
+		public bool AllowSolution { get; set; }
+		public ICollection<Tag> Tags { get; set; }
+	}
+}
---
+++ Crytomind.Core/Services/AdminService.cs
@@ -0,0 +1,72 @@
+﻿using Cryptomind.Data.Entities;
+using Crytomind.Core.Contracts;
+using Cryptomind.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cryptomind.Common.CipherAdminViewModels;
+
+namespace Crytomind.Core.Services
+{
+	public class AdminService (
+		IRepository<Cipher, int> cipherRepo) : IAdminService
+	{
+
+		public async Task<List<Cipher>> AllSubmittedCyphers()
+		{
+			var result = (await cipherRepo.GetAllAsync()).Where(c => c.IsApproved == false).ToList();
+			if (result == null) throw new InvalidOperationException("Wasn't able to retrieve submitted ciphers");
+			return result;
+		}
+		public async Task<Cipher> GetCipherById(int id) //Should I return cipher the current logic is to return it so it can be shown to the next view
+		{
+			Cipher? cipher = await cipherRepo.GetByIdAsync(id);
+
+			if (cipher == null) throw new InvalidOperationException("There is no cipher with the given Id");
+			return cipher; //Go to next
+		}
+
+		public async Task ApproveCipherAsync(int id, ApproveCipherViewModel model)
+		{
+			Cipher? cipher = await cipherRepo.GetByIdAsync(id);
+			if (cipher == null) throw new InvalidOperationException("There is no cipher with the given Id");
+
+			List<CipherTag> cipherTags = new List<CipherTag>();
+			foreach (var tag in model.Tags)
+			{
+				var cipherTag = new CipherTag()
+				{
+					TagId = tag.Id,
+					CipherId = cipher.Id,
+				};
+
+				cipherTags.Add(cipherTag);
+			}
+
+			cipher.CipherTags = cipherTags;
+
+			cipher.Title = model.Title;
+			cipher.DecryptedText = model.DecryptedText;
+			//result.TypeOfCipher = model.TypeOfCipher;
+			cipher.AllowHint = model.AllowHint;
+			cipher.AllowSolution = model.AllowSolution;
+			cipher.IsApproved = true;
+
+			bool isSuccessfull = await cipherRepo.UpdateAsync(cipher);
+            await Console.Out.WriteLineAsync(isSuccessfull.ToString());
+            //Here there might be name collision with other cipher
+        }
+
+		public async Task RejectCipherAsync(int id)
+		{
+			Cipher? cipher = await cipherRepo.GetByIdAsync(id);
+			if (cipher == null) throw new InvalidOperationException("There is no cipher with the given Id");
+
+			bool isDeleted = await cipherRepo.DeleteAsync(cipher);
+
+			if (!isDeleted) throw new InvalidOperationException("Wasn't able to delete the cipher");
+		}
+	}
+}
