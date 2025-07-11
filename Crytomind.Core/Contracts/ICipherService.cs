6e28fc57126b1a22626069dda30542839a5a159d
AdelinChaushev <106106846+AdelinChaushev@users.noreply.github.com>
2025-07-11 18:54:51 +0300

    Added cipher detail page and created a new migration.

--- Crytomind.Core/Contracts/ICipherService.cs
+++ Crytomind.Core/Contracts/ICipherService.cs
@@ -13,7 +13,7 @@ namespace Crytomind.Core.Contracts
 		Task<List<Cipher>> GetApprovedAsync(CipherFilter? filter); // Implement functionality to be able to filter by tags
 		Task<Cipher?> GetCipherAsync(int id);
 		Task<Cipher> SubmitCipherAsync(Cipher cipher);
-		Task<string> AnswerCipherAsync(string input, int cipherId);
+		Task<string> AnswerCipherAsync(string userId,string input, int cipherId);
 		Task<HintRequestResponse> RequestHintAsync(HintRequest request);
 	}
 }
