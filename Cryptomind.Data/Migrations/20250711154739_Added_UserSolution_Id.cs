610e42a8e90809d4d08012b85a4fd06776c0751a
AdelinChaushev <106106846+AdelinChaushev@users.noreply.github.com>
2025-07-11 18:54:51 +0300

    Added cipher detail page and created a new migration.

---
+++ Cryptomind.Data/Migrations/20250711154739_Added_UserSolution_Id.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cryptomind.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_UserSolution_Id : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserSolution",
+                table: "UserSolution");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "UserSolution",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserSolution",
+                table: "UserSolution",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSolution_CipherId",
+                table: "UserSolution",
+                column: "CipherId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserSolution",
+                table: "UserSolution");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserSolution_CipherId",
+                table: "UserSolution");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "UserSolution");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserSolution",
+                table: "UserSolution",
+                columns: new[] { "CipherId", "UserId" });
+        }
+    }
+}
