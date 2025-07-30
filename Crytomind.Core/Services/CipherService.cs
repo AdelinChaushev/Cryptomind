a6b81123c92421c41e7ee97274e8e4673998bd26
Samuil Gigov <samuilgigov@gmail.com>
2025-07-30 12:22:04 +0300

    Added validation for the uploaded files, fixed the searching filtration for ciphers.

--- Crytomind.Core/Services/CipherService.cs
+++ Crytomind.Core/Services/CipherService.cs
@@ -11,9 +11,9 @@ using System.Xml.Linq;
 
 namespace Crytomind.Core.Services
 {
-	public class CipherService (IRepository<Cipher, int> cipherRepo) : ICipherService
+	public class CipherService(IRepository<Cipher, int> cipherRepo) : ICipherService
 	{
-        public async Task<string> AnswerCipherAsync(string input, int cipherId)
+		public async Task<string> AnswerCipherAsync(string input, int cipherId)
 		{
 			Cipher cipher = await cipherRepo.GetByIdAsync(cipherId);
 			if (cipher == null) throw new InvalidOperationException("There is no cipher with the given Id");
@@ -30,13 +30,13 @@ namespace Crytomind.Core.Services
 		}
 		public async Task<List<Cipher>> GetApprovedAsync(CipherFilter? filter)
 		{
-			List<Cipher> approved = cipherRepo.GetAllAttached().ToList();
+			List<Cipher> approved = (await cipherRepo.GetAllAsync()).Where(c => c.IsApproved).ToList();
 
-			if(!string.IsNullOrEmpty(filter.SearchTerm))
-				approved.Where(c => c.Title.Contains(filter.SearchTerm));
+			if (!string.IsNullOrEmpty(filter.SearchTerm))
+				approved = approved.Where(c => c.Title.Contains(filter.SearchTerm)).ToList();
 
 			if (filter.Tags != null)
-				approved.Where(c => c.CipherTags.Any(t => filter.Tags.Contains(t.Tag.Type)));
+				approved = approved.Where(c => c.CipherTags.Any(t => filter.Tags.Contains(t.Tag.Type))).ToList();
 
 			return approved;
 		}
@@ -54,11 +54,29 @@ namespace Crytomind.Core.Services
 		}
 		public async Task<Cipher> SubmitCipherAsync(Cipher cipher)
 		{
-            if (cipherRepo.GetAllAttached().Where(c => c.Title == cipher.Title).Count() != 0)
+			if (cipherRepo.GetAllAttached().Where(c => c.Title == cipher.Title).Count() != 0)
 				throw new InvalidOperationException("Cannot create two ciphers with the same name");
+
+			ValidateImageFiles(cipher);
 
 			await cipherRepo.AddAsync(cipher);
 			return cipher;
+		}
+		private void ValidateImageFiles(Cipher cipher)
+		{
+			if (cipher is ImageCipher imageCipher)
+			{
+				var allowedExtensions = new List<string> { ".jpg", ".jpeg", ".png", ".webp" };
+
+				if (imageCipher.ImagePath != null)
+				{
+					var extension = Path.GetExtension(imageCipher.ImagePath.ToString()).ToLowerInvariant();
+					if (!allowedExtensions.Contains(extension))
+					{
+						throw new InvalidOperationException($"Invalid file type. Allowed types: {string.Join(", ", allowedExtensions)}");
+					}
+				}
+			}
 		}
 	}
 }
