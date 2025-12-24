75dd344aa032be1c0e417f1f3c223ec9583476b7
Samuil Gigov <samuilgigov@gmail.com>
2025-12-24 17:07:02 +0200

    Integrated the AI system into a service.

---
+++ Cryptomind.Core/Contracts/ICipherRecognizerService.cs
@@ -0,0 +1,15 @@
+﻿using Cryptomind.Common.CipherRecognitionViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptomind.Core.Contracts
+{
+	public interface ICipherRecognizerService
+	{
+		Task<CipherRecognitionResultViewModel> ClassifyCipherAsync (string inputText);
+		Task<bool> IsServiceHealthyAsync();
+	}
+}
