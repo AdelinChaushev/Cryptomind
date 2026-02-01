6d6f602785e7100f093920bff012caa2066559b6
Samuil Gigov <samuilgigov@gmail.com>
2026-02-01 12:55:40 +0200

    Implemented admin verification tool

--- Cryptomind.Common/AdminViewModels/ApproveCipherViewModel.cs
+++ Cryptomind.Common/AdminViewModels/ApproveCipherViewModel.cs
@@ -8,14 +8,14 @@ using System.Threading.Tasks;
 
 namespace Cryptomind.Common.CipherAdminViewModels
 {
-	public class ApproveUpdateCipherViewModel
+	public class ApproveCipherViewModel
 	{
 		public string Title { get; set; }
-		//public CipherType TypeOfCipher { get; set; }
-		public string EncryptedText { get; set; }
 		public bool AllowHint { get; set; }
 		public bool AllowSolution { get; set; }
-		public bool isStandard { get; set; }
+		public bool AllowTypeHint { get; set; }
+		public CipherType CipherType { get; set; }
+		public ChallengeType ChallengeType { get; set; }
 		public ICollection<int>? TagIds { get; set; }
 	}
 }
