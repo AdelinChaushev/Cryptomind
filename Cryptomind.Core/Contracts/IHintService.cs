43119a6bdc85ecdf033bace6c93f061b63dcb795
Samuil Gigov <samuilgigov@gmail.com>
2026-02-05 20:15:57 +0200

    Implemented a hint system

---
+++ Cryptomind.Core/Contracts/IHintService.cs
@@ -0,0 +1,14 @@
+﻿using Cryptomind.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptomind.Core.Contracts
+{
+	public interface IHintService
+	{
+		Task<string> RequestHintAsync(string userId, int cipherId, HintType hintType);
+	}
+}
