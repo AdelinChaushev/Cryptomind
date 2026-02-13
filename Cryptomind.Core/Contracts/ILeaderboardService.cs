459454fbda0b953f1ad4048ac21e2b50b34257c8
Samuil Gigov <samuilgigov@gmail.com>
2026-02-13 17:22:57 +0200

    Leaderboard

---
+++ Cryptomind.Core/Contracts/ILeaderboardService.cs
@@ -0,0 +1,14 @@
+﻿using Cryptomind.Common.ViewModels.LeaderboardPlaceViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptomind.Core.Contracts
+{
+	public interface ILeaderboardService
+	{
+		Task<List<LeaderboardPlaceViewModel>> GetLeaderboard();
+	}
+}
