d39b1983e8836d50237460e3509cac86447f5f8c
Samuil Gigov <samuilgigov@gmail.com>
2026-02-15 16:01:54 +0200

    Added admin dashboard, fixed ordering for most popular

---
+++ Cryptomind.Common/ViewModels/AdminViewModels/DashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptomind.Common.ViewModels.AdminViewModels
+{
+	public class DashboardViewModel
+	{
+		public int PendingCiphersCount { get; set; }
+		public int ApprovedCiphersCount { get; set; }
+		public int PendingAnswersCount { get; set; }
+		public int ApprovedAnswersCount { get; set; }
+		public List<string> PendingCipherTitles { get; set; } 
+	}
+}
