35207259ed657ae8bf2c2910de9e6fcf7517158b
Samuil Gigov <samuilgigov@gmail.com>
2026-02-17 17:08:33 +0200

    Fixed deactivation, added user filtering for admins, cleared code.

---
+++ Cryptomind.Common/DTOs/UserFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptomind.Common.DTOs
+{
+	public class UserFilter
+	{
+		public bool? IsBanned { get; set; }
+		public bool? IsDeactivated { get; set; }
+	}
+}
