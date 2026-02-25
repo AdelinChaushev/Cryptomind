9f525cb1a619075e014bcd5231cda9e84da9b087
Samuil Gigov <samuilgigov@gmail.com>
2026-02-25 19:18:00 +0200

    Updating available points on hint requesting.

---
+++ Cryptomind.Common/DTOs/HintResultDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptomind.Common.DTOs
+{
+	public class HintResultDTO
+	{
+		public string HintContent { get; set; }
+		public int AvailablePoints { get; set; }
+	}
+}
