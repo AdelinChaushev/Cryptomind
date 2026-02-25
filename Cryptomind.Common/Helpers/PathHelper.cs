db9812a21921fee77cebe2d7f15a8bd6a95b8510
Samuil Gigov <samuilgigov@gmail.com>
2026-02-25 18:45:56 +0200

    Fix the docker images

---
+++ Cryptomind.Common/Helpers/PathHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace Cryptomind.Common.Helpers
+{
+	public static class PathHelper
+	{
+		public static string GetImagesBasePath()
+		{
+			if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+				return Path.Combine(AppContext.BaseDirectory, "Images");
+			else
+				return Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "Images"));
+		}
+	}
+}
