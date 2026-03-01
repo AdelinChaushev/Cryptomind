9fa64679925111ba452c7ec545ce40165673c612
AdelinChaushev <106106846+AdelinChaushev@users.noreply.github.com>
2026-03-01 17:31:42 +0200

    Fixed issue in require auth.

--- cryptomind.js/src/RequiresNotBanned.jsx
+++ cryptomind.js/src/RequiresNotBanned.jsx
@@ -4,12 +4,10 @@ import { AuthorizationContext } from './App.jsx';
 
 export default function RequireNotBanned({ mustNotBeBanned = false,children }) {
     const {state}  = useContext(AuthorizationContext);
-     if (state.isBanned && mustNotBeBanned) { 
-        console.log("User is not banned, rendering children." + " isBanned: " + state.isBanned + ", mustNotBeBanned: " + mustNotBeBanned);
-        return <Navigate to="/banned" replace />;
+     if (state.isBanned && mustNotBeBanned) {      
+       return <Navigate to="/banned" replace />;
      }        
     else if(!mustNotBeBanned && !state.isBanned){
-        console.log("User is not banned, rendering children." + " isBanned: " + state.isBanned + ", mustNotBeBanned: " + mustNotBeBanned);
         return <Navigate to="/" replace />;
     } 
     else{
