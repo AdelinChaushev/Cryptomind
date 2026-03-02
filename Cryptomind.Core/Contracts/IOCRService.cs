a5ede3ea414a4fab9c58c0a64aec7e80a6543a12
Samuil Gigov <samuilgigov@gmail.com>
2026-03-02 14:46:35 +0200

    Cleared OCR

--- Cryptomind.Core/Contracts/IOCRService.cs
+++ Cryptomind.Core/Contracts/IOCRService.cs
@@ -6,7 +6,6 @@ namespace Cryptomind.Core.Contracts
 	public interface IOCRService
 	{
 		Task<OCRResultDTO> ExtractTextFromImageAsync(IFormFile imageFile);
-		Task<OCRResultDTO> ExtractTextWithMultipleMethodsAsync(IFormFile imageFile);
 		Task<bool> IsServiceHealthyAsync();
 	}
 }
