82ca8dbf8897539e1b87707da520cebd4d0bcd8c
Samuil Gigov <samuilgigov@gmail.com>
2026-03-07 10:23:32 +0200

    Cleared Cryptomind.AI comments

--- Cryptomind.OCR/services/__init__.py
+++ Cryptomind.OCR/services/__init__.py
@@ -1,6 +1,3 @@
-# This file makes the services directory a Python package
-# It can be empty or contain package-level imports
-
 from .ocr_service import OCRService
 from .image_preprocessor import ImagePreprocessor
 
