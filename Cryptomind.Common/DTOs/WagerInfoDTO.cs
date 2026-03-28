ae09332817affa5be5cef9522242757a89e7e6e7
Samuil Gigov <samuilgigov@gmail.com>
2026-03-28 19:34:15 +0200

    Implemented wager system for race room

---
+++ Cryptomind.Common/DTOs/WagerInfoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Cryptomind.Common.DTOs
+{
+	public class WagerInfoDTO
+	{
+		public int WagerAmount { get; set; }
+		public string CreatorUsername { get; set; } = string.Empty;
+		public int JoinerBalance { get; set; }
+	}
+}
