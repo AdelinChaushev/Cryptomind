c07886e5f45232c3401dc141299ca219b1337a5e
Samuil Gigov <samuilgigov@gmail.com>
2026-03-28 19:34:15 +0200

    Implemented wager system for race room

--- Cryptomind.Core/Contracts/IRoomService.cs
+++ Cryptomind.Core/Contracts/IRoomService.cs
@@ -5,15 +5,18 @@ namespace Cryptomind.Core.Contracts
 {
 	public interface IRoomService
 	{
-		Task<string> CreateRoom(string creatorId);
+		Task<string> CreateRoom(string creatorId, int wagerAmount);
 		void RemoveRoom(string roomCode);
 		bool RoomExists(string roomCode);
 		string? GetRoomCodeForPlayer(string userId);
 		GameStateDTO? GetPlayerGameState(string userId);
 		(string player1Id, string? player2Id)? GetPlayerIds(string roomCode);
 		Task<(string player1Username, string player2Username)> GetPlayerUsernames(string roomCode);
+		Task<WagerInfoDTO> GetWagerInfo(string roomCode, string joinerId);
 		Task<bool> JoinRoom(string roomCode, string userId);
 		Task<bool> SetReady(string roomCode, string userId);
+		Task LockWagers(string roomCode);
+		Task RefundWagers(string roomCode);
 		bool IsPlayerInRoom(string roomCode, string userId);
 		string StartRoom(string roomCode);
 		Task<RoomSubmissionResultDTO> SubmitAnwer(string roomCode, string userId, CipherType answer);
